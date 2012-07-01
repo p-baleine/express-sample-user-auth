4930e4a03e840f2f70493bba54308fd56b5701dd
Tajima Junpei <p.baleine@gmail.com>
2012-07-01 18:10:47 +0900

    modify bug

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ function requireLogin(req, res, next) {
   if (!req.url.match(/login/) && !req.user) {
     req.flash("info", "please login");
     res.redirect('/login');
+    return;
   }
   next();
 }
